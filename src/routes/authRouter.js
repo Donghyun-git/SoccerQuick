4441a83af81853d2d2e775a49e9dfa7ce58a5bb4
andonghyun <ax34554@gmail.com>
2023-05-28 00:04:02 +0900

    feat: [관리자] 유저 로그인 정지 라우터 추가

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -15,7 +15,10 @@ router.post('/login', userController.logIn);
 /* PATCH */
 
 //[ 회원정보 수정 ]
-router.patch('/update', userController.updateUserInfo);
+router.patch('/my/update', userController.updateUserInfo);
+
+//[ 관리자 - 유저 로그인 정지 ]
+router.patch('/admin/ban', userController.adminBanUser);
 
 /* DELETE */
 
