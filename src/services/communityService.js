f811f7e6b1302cc116bb18ba84620dfbfa4dbb7a
andonghyun <ax34554@gmail.com>
2023-05-27 18:15:42 +0900

    style: [ 주석 추가 ] : 5

--- src/services/communityService.js
+++ src/services/communityService.js
@@ -2,6 +2,7 @@ const { Post, Comment } = require('../model/models/index');
 const { AppError } = require('../middlewares/errorHandler');
 
 //[ 모든 게시글 조회 ]
+/** (컨트롤러에서 필요한 데이터를 받아 파라미터 넘겨서 로직 수행하고 반환해야함) */
 const getAllPosts = () => {};
 module.exports = {
   getAllPosts,
