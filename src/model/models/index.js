7f357e94ebf36580c7f88705a46f46847bd022be
andonghyun <ax34554@gmail.com>
2023-05-27 17:24:01 +0900

    feat: [ 리뷰, 커뮤니티, 풋살장 관련 모델 생성 ]

--- src/model/models/index.js
+++ src/model/models/index.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
+// [ 유저 모델 ]
 const UserSchema = require('../schemas/userSchema');
-
 const User = mongoose.model('User', UserSchema);
+
+// [ 커뮤니티 관련 모델 ]
+const { PostSchema, CommentSchema } = require('../schemas/communitySchema');
+const Post = mongoose.model('Post', PostSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
+
+// [ 리뷰 관련 모델 ]
+const ReviewSchema = require('../schemas/reviewSchema');
+const Review = mongoose.model('Review', ReviewSchema);
+
+// [ 풋살장 관련 모델 ]
+const GroundSchema = require('../schemas/groundSchema');
+const Ground = mongoose.model('Ground', GroundSchema);
 
 module.exports = {
   User,
+  Post,
+  Comment,
+  Review,
+  Ground,
 };
