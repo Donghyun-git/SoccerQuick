6d174654b420ee500f2184ce91c4ee895147c748
신성민 <seongmin@sinseongmin-ui-MacBookPro.local>
2023-05-29 00:51:21 +0900

    create: 풋살장 라우터, 컨트롤러, 서비스 파일 생성

---
+++ src/routes/groundRouter.js
@@ -0,0 +1,12 @@
+const { Router } = require('experss');
+const router = Router();
+const groundController = require('../controllers/groundController');
+
+/* GET */
+// [모든 구장 조회]
+router.get('/', groundController.getAllGrounds);
+
+// [위치, 날짜 구장 필터링 ]
+router.get('/filter', groundController.getFilteredGrounds);
+
+module.exports = router;
