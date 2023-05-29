2b47ed41f2f8d11a8b084a022cfa8c5879d13852
andonghyun <ax34554@gmail.com>
2023-05-29 23:15:33 +0900

    fix: 커스텀 인덱싱 리턴값 변경

--- src/utils/createPostId.js
+++ src/utils/createPostId.js
@@ -2,7 +2,7 @@
 /** (현재 post 게시글 배열 객체) */
 const createPostId = (posts) => {
   const index = posts.length + 1;
-  return `post_${index}`;
+  return `post${index}`;
 };
 
 module.exports = createPostId;
