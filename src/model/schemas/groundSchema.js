b0e8f09356ae33048beba34b4529b7c77103b2d7
seongm2n <seongm22n@gmail.com>
2023-05-30 15:44:24 +0900

    fix: ground 스키마 필드 변경

--- src/model/schemas/groundSchema.js
+++ src/model/schemas/groundSchema.js
@@ -2,6 +2,7 @@ const { Schema } = require('mongoose');
 
 const GroundSchema = new Schema(
   {
+    groundId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     location: {
       latitude: { type: Number, required: true },
