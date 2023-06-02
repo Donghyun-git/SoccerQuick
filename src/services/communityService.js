70e6bdf61cfebebafab1c40a500a2ab868bbf933
andonghyun <ax34554@gmail.com>
2023-06-03 03:07:01 +0900

    feat: 댓글 crud 서비스 로직

--- src/services/communityService.js
+++ src/services/communityService.js
@@ -1,6 +1,6 @@
 const { Post, Comment, User, Admin } = require('../model/models/index');
 const { AppError } = require('../middlewares/errorHandler');
-const { createPostId } = require('../utils/createIndex');
+const { createPostId, createCommentId } = require('../utils/createIndex');
 const toString = require('../utils/toString');
 
 // [ 커뮤니티 전체 게시글 조회 ]
@@ -32,6 +32,43 @@ const addPost = async (posts) => {
 
     if (notice === '공지사항' && !admin_id)
       return new AppError(403, '관리자만 공지사항을 등록할 수 있습니다.');
+
+    if (foundUser.community_banned) {
+      const { community_banEndDate } = foundUser;
+      const currentDate = new Date();
+
+      if (community_banEndDate && community_banEndDate <= currentDate) {
+        foundUser.community_banned = false;
+        foundUser.community_banEndDate = null;
+
+        await foundUser.save();
+      } else {
+        const dateString = community_banEndDate.toString();
+        const newDate = new Date(dateString);
+
+        const options = {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          weekday: 'long',
+          timeZoneName: 'long',
+        };
+
+        const dateFormatter = new Intl.DateTimeFormat('ko-KR', options);
+        const translatedDate = dateFormatter.format(newDate);
+
+        const [year, month, date, day, type, hour, minute] =
+          translatedDate.split(' ');
+
+        return new AppError(
+          403,
+          `${year} ${month} ${date} ${day} ${type} ${hour} ${minute} 까지 커뮤니티 이용 정지입니다.`
+        );
+      }
+    }
 
     const userObjectId = foundUser._id;
 
@@ -68,19 +105,19 @@ const updatePost = async (post) => {
     if (notice === '공지사항' && !foundUser.admin_id)
       return new AppError(403, '관리자만 공지사항으로 변경 가능합니다.');
 
-    const user_id = foundUser._id;
+    const userObjectId = foundUser._id;
 
     const foundPost = await Post.findOne({ post_id: postId });
 
     if (!foundPost) return new AppError(400, '존재하지 않는 게시물입니다.');
 
-    if (toString(user_id) !== toString(foundPost.user_id)) {
+    if (toString(userObjectId) !== toString(foundPost.user_id)) {
       return new AppError(400, '본인이 작성한 게시글만 수정 가능합니다.');
     }
 
     const updatedPostObj = {
       post_id: foundPost.post_id,
-      user_id: user_id,
+      user_id: userObjectId,
       title: title,
       description: description,
       notice: notice,
@@ -133,10 +170,129 @@ const deletePost = async (post_id, userId) => {
 };
 
 //[ 커뮤니티 댓글 등록 ]
+const addComment = async (postId, user_id, content) => {
+  try {
+    const foundUser = await User.findOne({ user_id });
+
+    if (!foundUser) return new AppError(404, '존재하지 않는 사용자입니다.');
+
+    const userObjectId = foundUser._id;
+
+    const foundPost = await Post.findOne({ post_id: postId });
+
+    if (!foundPost) return new AppError(404, '존재하지 않는 게시글입니다.');
+
+    const postObjectId = foundPost._id;
+
+    const comment_id = await createCommentId();
+
+    const createComment = await Comment.create({
+      comment_id,
+      user_id: userObjectId,
+      post_id: postObjectId,
+      content,
+    });
+
+    const commentObjectId = createComment._id;
+
+    foundPost.comments.push(commentObjectId);
+
+    await foundPost.save();
+
+    return { message: '댓글이 등록되었습니다.', data: createComment };
+  } catch (error) {
+    console.error(error);
+    return new AppError(500, '댓글 등록 실패');
+  }
+};
+
+// [ 커뮤니티 댓글 수정 ]
+const updateComment = async (comment) => {
+  const { postId, commentId, user_id, content } = comment;
+
+  const foundPost = await Post.findOne({ post_id: postId });
+  if (!foundPost) return new AppError(404, '존재하지 않는 게시글 입니다.');
+
+  const postCommentsArray = foundPost.comments;
+
+  const foundComment = await Comment.findOne({ comment_id: commentId });
+  if (!foundComment) return new AppError(404, '존재하지 않는 댓글입니다.');
+
+  const commnetObjectId = foundComment._id;
+  const commentUserId = toString(foundComment.user_id);
+
+  const foundUser = await User.findOne({ user_id });
+  if (!foundUser) return new AppError(404, '존재하지 않는 사용자입니다.');
+
+  const userObjectId = toString(foundUser._id);
+
+  if (commentUserId !== userObjectId)
+    return new AppError(403, '댓글 작성자만 수정 가능합니다.');
+
+  const foundUserComment = postCommentsArray.find(
+    (comment) => toString(comment) === toString(commnetObjectId)
+  );
+
+  if (!foundUserComment)
+    return new AppError(404, '댓글이 삭제되었거나 존재하지 않습니다!');
+
+  const updateCommentObj = {
+    content: content,
+  };
+
+  const updateComment = await Comment.findOneAndUpdate(
+    { comment_id: commentId },
+    { $set: updateCommentObj },
+    { new: true }
+  );
+
+  return { message: '댓글 수정 성공', data: updateComment };
+};
+
+// [ 커뮤니티 댓글 삭제 ]
+const deleteComment = async (comment) => {
+  const { postId, commentId, user_id } = comment;
+
+  const foundPost = await Post.findOne({ post_id: postId });
+  if (!foundPost) return new AppError(404, '존재하지 않는 게시글 입니다.');
+
+  const postCommentsArray = foundPost.comments;
+
+  const foundComment = await Comment.findOne({ comment_id: commentId });
+  if (!foundComment) return new AppError(404, '존재하지 않는 댓글입니다.');
+
+  const commnetObjectId = foundComment._id;
+  const commentUserId = toString(foundComment.user_id);
+
+  const foundUser = await User.findOne({ user_id });
+  if (!foundUser) return new AppError(404, '존재하지 않는 사용자입니다.');
+
+  const userObjectId = toString(foundUser._id);
+
+  if (commentUserId !== userObjectId)
+    return new AppError(403, '댓글 작성자만 삭제 가능합니다.');
+
+  const foundUserComment = postCommentsArray.find(
+    (comment) => toString(comment) === toString(commnetObjectId)
+  );
+
+  if (!foundUserComment)
+    return new AppError(404, '댓글이 삭제되었거나 존재하지 않습니다!');
+
+  const updateComment = await Comment.deleteOne({ comment_id: commentId });
+
+  foundPost.comments.pull(commnetObjectId);
+  await foundPost.save();
+
+  return { message: '댓글 삭제 성공' };
+};
 
 module.exports = {
   addPost,
   getAllPosts,
   updatePost,
   deletePost,
+  addComment,
+  updateComment,
+  deleteComment,
 };
