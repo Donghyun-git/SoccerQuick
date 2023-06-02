caa9942946566a87a35771fb1b51ce7dff7491cf
andonghyun <ax34554@gmail.com>
2023-06-03 03:07:35 +0900

    feat: 댓글 crud 라우트 경로 추가

--- src/routes/communityRouter.js
+++ src/routes/communityRouter.js
@@ -10,12 +10,21 @@ router.get('/', communityController.getAllPosts);
 // [ 커뮤니티 게시글 등록 ]
 router.post('/', communityController.addPost);
 
+//[ 커뮤니티 게시글 댓글 등록 ]
+router.post('/:postId/comment', communityController.addComment);
+
 /* PATCH */
+//[ 커뮤니티 게시글 댓글 수정]
+router.patch('/:postId/comment/:commentId', communityController.updateComment);
+
 // [ 커뮤니티 게시글 수정 ]
 router.patch('/:postId', communityController.updatePost);
 
 /* DELETE*/
 // [ 커뮤니티 게시글 삭제 ]
 router.delete('/:postId', communityController.deletePost);
+
+//[ 커뮤니티 게시글 댓글 삭제 ]
+router.delete('/:postId/comment/:commentId', communityController.deleteComment);
 
 module.exports = router;
