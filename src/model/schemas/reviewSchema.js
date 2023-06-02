6f3c1be5f2ec146da0df76eadbfbf60d0c825cf2
seongm2n <seongm22n@gmail.com>
2023-06-03 04:11:37 +0900

    feat: review_id 추가

--- src/model/schemas/reviewSchema.js
+++ src/model/schemas/reviewSchema.js
@@ -3,6 +3,10 @@ const { Schema } = require('mongoose');
 // Review 스키마
 const ReviewSchema = new Schema(
   {
+    review_id: {
+      type: String,
+      required: true,
+    },
     user_id: {
       type: Schema.Types.ObjectId,
       ref: 'User',
