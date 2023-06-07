50e549dcf86be5799bc27eda53b8bac258f03541
seongm2n <seongm22n@gmail.com>
2023-06-07 23:31:11 +0900

    fix: conflict 해결

--- src/controllers/groundController.js
+++ src/controllers/groundController.js
@@ -4,9 +4,16 @@ const { AppError } = require('../middlewares/errorHandler');
 // [ 전체 구장 조회 ]
 const getAllGrounds = async (req, res, next) => {
   try {
-    const grounds = await groundService.getAllGrounds();
-    res.status(200).json(grounds);
+    const result = await groundService.getAllGrounds();
+    if (result.statusCode !== 200)
+      return next(new AppError(result.statusCode, result.message));
+
+    res.status(200).json({
+      message: result.message,
+      data: result.data,
+    });
   } catch (error) {
+    console.error(error);
     return next(new AppError(500, 'Internal Server Error'));
   }
 };
@@ -15,10 +22,10 @@ const getAllGrounds = async (req, res, next) => {
 const getFilteredGrounds = async (req, res, next) => {
   try {
     const { location, date } = req.query;
-    const filteredGrounds = await groundService.getFilteredGrounds(
+    const filteredGrounds = await groundService.getFilteredGrounds({
       location,
-      date
-    );
+      date,
+    });
     res.json(filteredGrounds);
   } catch (error) {
     return next(new AppError(500, 'Internal Server Error'));
@@ -52,7 +59,7 @@ const removeFavorites = async (req, res, next) => {
   const { userId } = req.body;
 
   try {
-    const result = await groundService.removeFavorites(groundId, userId);
+    const result = await groundService.removeFavorites({ groundId, userId });
 
     if (result.statusCode !== 204)
       return next(new AppError(result.statusCode, result.message));
@@ -60,6 +67,19 @@ const removeFavorites = async (req, res, next) => {
     res.status(204).json({
       message: result.message,
     });
+  } catch (error) {
+    console.error(error);
+    return next(new AppError(500, 'Internal Server Error'));
+  }
+};
+
+// [ 상세페이지 - 풋살장 예약 ]
+const reserveGround = async (req, res, next) => {
+  const { ground_id } = req.params;
+  const { 예약정보 } = req.body;
+
+  try {
+    const result = await groundService.reserveGround({ ground_id, 예약정보 });
   } catch (error) {
     console.error(error);
     return next(new AppError(500, 'Internal Server Error'));
