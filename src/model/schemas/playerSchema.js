72bbdbd5bc6009322dff76843e44a9ef608c79c8
andonghyun <ax34554@gmail.com>
2023-06-08 23:14:46 +0900

    fix: playerSchema, 유저 네임 필드 추가

--- src/model/schemas/playerSchema.js
+++ src/model/schemas/playerSchema.js
@@ -2,6 +2,7 @@ const { Schema } = require('mongoose');
 
 const playerSchema = {
   id: { type: Schema.Types.ObjectId, ref: 'User' },
+  name: { type: String, ref: 'User.name' },
   gender: { type: String, ref: 'User.gender' },
   position: { type: String, required: true },
   level: { type: String, required: true },
