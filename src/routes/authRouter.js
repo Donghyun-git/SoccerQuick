e2790d2194f264e402025d961438e927492307bd
andonghyun <ax34554@gmail.com>
2023-06-09 19:56:45 +0900

    style: 로그아웃 api 메서드 delete로 변경

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -9,14 +9,13 @@ router.post('/signup', authController.signUp);
 // [ 로그인 ]
 router.post('/login', authController.logIn);
 
-//[ 로그아웃 ]
-
-router.post('/logout', tokenValidator, authController.logOut);
-
 //[회원가입 아이디 중복체크]
 router.post('/id', authController.validateUniqueUserId);
 
 // [ 비밀번호 체크 ]
 router.post('/password', authController.validatePassword);
+
+//[ 로그아웃 ]
+router.delete('/logout', tokenValidator, authController.logOut);
 
 module.exports = router;
